c530cb7d7c2e780bd8cd053f788ec87c599fcd75
Milan George <milangeorge98@gmail.com>
2022-01-28 00:31:49 -0600

    Time: 854 ms (25.91%), Space: 15.3 MB (37.50%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dest=len(nums)-1
+        
+        for i in range(len(nums)-1,-1,-1):
+            if i +nums[i]>=dest:
+                dest=i
+        
+        return True if dest==0 else False
