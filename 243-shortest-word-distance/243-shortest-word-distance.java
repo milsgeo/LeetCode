fa769a80be09a6bf4c9f51cef68a967b82ef73ff
Milan George <milangeorge98@gmail.com>
2022-01-28 01:27:43 -0600

    Time: 3 ms (35.09%), Space: 45.1 MB (5.49%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -1,15 +1,31 @@
 class Solution {
     public int shortestDistance(String[] wordsDict, String word1, String word2) {
-        int minDist=wordsDict.length;
-        for(int i=0;i<wordsDict.length;i++){
-            if(wordsDict[i].equals(word1)){
-                for(int j=0;j<wordsDict.length;j++){
-                    if(wordsDict[j].equals(word2)){
-                        minDist=Math.min(minDist,Math.abs(i-j));
-                    }
-                }
+        // int minDist=wordsDict.length;
+        // for(int i=0;i<wordsDict.length;i++){
+        //     if(wordsDict[i].equals(word1)){
+        //         for(int j=0;j<wordsDict.length;j++){
+        //             if(wordsDict[j].equals(word2)){
+        //                 minDist=Math.min(minDist,Math.abs(i-j));
+        //             }
+        //         }
+        //     }
+        // }
+        // return minDist;
+        
+        //Two-pointer approach
+        int i1 = -1, i2 = -1;
+        int minDistance = wordsDict.length;
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                i1 = i;
+            } else if (wordsDict[i].equals(word2)) {
+                i2 = i;
+            }
+
+            if (i1 != -1 && i2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(i1 - i2));
             }
         }
-        return minDist;
+        return minDistance;
     }
 }
