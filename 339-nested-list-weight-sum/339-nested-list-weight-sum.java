03d9645e5b6a01f1a9a2c04bd40e0e9409ed2380
Milan George <milangeorge98@gmail.com>
2022-01-28 20:32:34 -0600

    Time: 1 ms (56.71%), Space: 42.7 MB (7.59%) - LeetHub

--- 339-nested-list-weight-sum/339-nested-list-weight-sum.java
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.java
@@ -26,21 +26,45 @@
  *     public List<NestedInteger> getList();
  * }
  */
-class Solution {
-    public int depthSum(List<NestedInteger> nestedList) {
-        return dfs(nestedList,1);
+// class Solution {
+//     public int depthSum(List<NestedInteger> nestedList) {
+//         return dfs(nestedList,1);
         
-    }
+//     }
     
-    private int dfs(List<NestedInteger> list, int depth){
+//     private int dfs(List<NestedInteger> list, int depth){
+//         int total=0;
+//         for(NestedInteger nested:list){
+//             if(nested.isInteger()){
+//                 total+= nested.getInteger()*depth;
+//             }else{
+//                 total+=dfs(nested.getList(),depth+1);
+//             }
+//         }
+//         return total;
+//     }
+// }
+
+class Solution{
+    public int depthSum(List<NestedInteger> nestedList){
+        Queue<NestedInteger> queue = new LinkedList<>();
+        queue.addAll(nestedList);
+        
+        int depth=1;
         int total=0;
-        for(NestedInteger nested:list){
-            if(nested.isInteger()){
-                total+= nested.getInteger()*depth;
-            }else{
-                total+=dfs(nested.getList(),depth+1);
+        
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                NestedInteger nested=queue.poll();
+                if(nested.isInteger()){
+                    total+=nested.getInteger()*depth;
+                }else{
+                    queue.addAll(nested.getList());
+                }
             }
-        }
-        return total;
+            depth++;
+        }       
+        return total;        
     }
 }
