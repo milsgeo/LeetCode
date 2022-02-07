cad90ef57d84945a8443536723ca1812f596d188
Milan George <milangeorge98@gmail.com>
2022-02-07 11:38:10 -0600

    Time: 264 ms (24.77%), Space: 15.7 MB (41.52%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.py
+++ 696-count-binary-substrings/696-count-binary-substrings.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         groups=[1]
-        for i in xrange(1,len(s)):
+        for i in range(1,len(s)):
             if(s[i-1]!=s[i]):
                 groups.append(1)
             else:
@@ -13,6 +13,6 @@ class Solution(object):
             
         count=0
         
-        for i in xrange(1,len(groups)):
+        for i in range(1,len(groups)):
             count+=min(groups[i-1],groups[i])
         return count
