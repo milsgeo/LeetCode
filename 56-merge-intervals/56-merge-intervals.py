d1e8587402e44ae235be6258c3cb190d345073ec
Milan George <milangeorge98@gmail.com>
2022-09-19 22:44:09 -0500

    Time: 159 ms (88.87%), Space: 18.8 MB (26.44%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,37 +1,13 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        # # newArr=[]
-        # l=len(intervals)
-        # # newele=[]
-        # for i in range(l-1):
-        #     # for j in range(l-1):
-        #     if(intervals[i][1]>=intervals[i+1][0]):
-        #         newele=[intervals[i][0], intervals[i+1][1]]
-        #         # return [[intervals[i][0], intervals[i+1][1]]]
-        #         intervals[i]=intervals[newele]
-        #         intervals.pop(i+1)
-        # # newArr.append(newele)
-        # # if(l>2):
-        # #     newArr.extend(intervals[i:]) 
-        # # else:
-        # #     return newArr
-        # # return newArr
-        # return intervals 
-        
-        intervals.sort(key=lambda i:i[0])
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if (len(intervals)<2):
+            return intervals
+        intervals.sort()
         output=[intervals[0]]
         
-        for start,end in intervals[1:]:
-            lastend=output[-1][1]
-            # print(lastend)
-            if start <=lastend:
-                #end value of the most recently added interval
-                output[-1][1]=max(lastend,end)
-                # print(output[-1][1])
-            else:
-                output.append([start,end])
+        for l,r in intervals[1:]:
+            if l > output[-1][1]:
+                output.append([l,r])
+            elif r > output[-1][1]:
+                output[-1][1]=r
         return output
