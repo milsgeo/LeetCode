8f785e8530d6510d027444829deb13b2eba03e31
Milan George <milangeorge98@gmail.com>
2022-09-19 22:26:09 -0500

    Time: 63 ms (32.06%), Space: 13.9 MB (15.77%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,56 +1,21 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
+        red:0
+        white:1
+        blue:2
         """
-        # for i in range(len(nums)):
-        #     while(i<len(nums)):
-        #         if(nums[i]<nums[i+1]):
-        #             nums[i], nums[i+1]=nums[i+1],nums[i]
+        n=len(nums)
+        red, white, blue=0,0,n-1
         
-#         l=0
-#         h=len(nums)-1
-        
-        
-#         while(l<=h):
-#             if(nums[l]<nums[h]):
-#                 l+=1
-#             elif(nums[l]>nums[h]):
-#                 # nums[l],nums[h]=nums[l],nums[h]
-#                 temp=nums[l]
-#                 nums[l]=nums[h]
-#                 nums[h]=temp
-#                 # l+=1
-#                 h-=1
-#             else:
-#                 l+=1
-#                 h+=1
-#         return nums     
-
-#         l=0
-#         h=l+1
-#         length=len(nums)
-        
-#         # while(l<=h and l<length and h<length):
-#         #     if(nums[l]<=nums[h]):
-#         #         l+=1
-#         #         h+=1
-#         #     else:
-#         #         temp=nums[l]
-#         #         nums[l]=nums[h]
-#         #         nums[h]=temp
-#         #         l+=1
-#         #         h+=1
-#         minNum=nums[0]
-#         for i in range(length):
-#             if(nums[i]<minNum):
-#                 minNum=nums[i]
-
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if(nums[i]>nums[j]):
-                    nums[i], nums[j]= nums[j], nums[i]
-        
-        return nums
-        
+        while white<=blue:
+            if(nums[white]==0):
+                nums[white], nums[red]=nums[red], nums[white]
+                white +=1
+                red +=1
+            elif(nums[white]==1):
+                white +=1
+            else:
+                nums[white], nums[blue]=nums[blue],nums[white]
+                blue-=1
