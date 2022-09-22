ae639873d860e26e003297472146b2c92c1c866c
Milan George <milangeorge98@gmail.com>
2022-09-21 21:14:53 -0500

    Time: 72 ms (5.27%), Space: 13.9 MB (64.01%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        pas=[[1]]
+        for i in range(1, rowIndex+1):
+            tmp=[1 for _ in range(i+1)]
+            for j in range(1, len(tmp)-1): #we only want the middle portion
+                tmp[j]=pas[-1][j]+pas[-1][j-1]
+            pas.append(tmp)
+        return pas[-1]
+            
