7d38f24ea1cd1dd2d8fd96d3b2cc810789e592ae
Milan George <milangeorge98@gmail.com>
2022-10-05 18:38:16 -0500

    Time: 1 ms (100.00%), Space: 50.1 MB (98.83%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,21 +1,35 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int N= nums.length;
-        int []leftProd=new int [N];
-        int []rightProd=new int [N];
+//         int N= nums.length;
+//         int []leftProd=new int [N];
+//         int []rightProd=new int [N];
         
-        int []outputArr=new int[N];
-        leftProd[0]=1;
-        rightProd[N-1]=1;
+//         int []outputArr=new int[N];
+//         leftProd[0]=1;
+//         rightProd[N-1]=1;
         
-        for(int i=1;i<N;i++){
-            leftProd[i]=nums[i-1]*leftProd[i-1];
+//         for(int i=1;i<N;i++){
+//             leftProd[i]=nums[i-1]*leftProd[i-1];
+//         }
+//         for(int i=N-2; i>=0;i--){
+//             rightProd[i]=nums[i+1]*rightProd[i+1];
+//         }
+//         for(int i=0; i<N;i++){
+//             outputArr[i]=leftProd[i]*rightProd[i];
+//         }
+//         return outputArr;
+        
+        //without extra space
+        int N=nums.length;
+        int[] outputArr= new int[N];
+        outputArr[0]=1;
+        for(int i=1; i<N;i++){
+            outputArr[i]=nums[i-1]*outputArr[i-1];
         }
-        for(int i=N-2; i>=0;i--){
-            rightProd[i]=nums[i+1]*rightProd[i+1];
-        }
-        for(int i=0; i<N;i++){
-            outputArr[i]=leftProd[i]*rightProd[i];
+        int R=1;
+        for(int i=N-1;i>=0;i--){
+            outputArr[i]=outputArr[i]*R;
+            R=R*nums[i];
         }
         return outputArr;
     }
