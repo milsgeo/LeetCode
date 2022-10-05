ebefba083cc5a5fd9a5a2debffbc81e8ba0336c0
Milan George <milangeorge98@gmail.com>
2022-10-05 15:23:14 -0500

    Time: 66 ms (8.45%), Space: 13.8 MB (73.71%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        map_dict={}
+        words=s.split()
+        if len(pattern)!=len(words):
+            return False
+        
+        for i in range(len(words)):
+            ch=pattern[i]
+            w=words[i]
+            
+            char_key = 'char_{}'.format(ch)
+            char_word = 'word_{}'.format(w)
+            
+            
+            if char_key not in map_dict:
+                map_dict[char_key]=i
+            if char_word not in map_dict:
+                map_dict[char_word]=i
+            if map_dict[char_key]!=map_dict[char_word]:
+                return False
+        return True
